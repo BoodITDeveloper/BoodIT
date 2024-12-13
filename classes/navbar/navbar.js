0c357c568f0b98d28a3ffd3987edc95893df1df4
Lead Developer <BoodIT_Development@hotmail.com>
2024-12-13 12:26:16 +0100

    first commit

---
+++ classes/navbar/navbar.js
@@ -0,0 +1,13 @@
+const navbarPlaceholder = document.getElementById("navbar");
+
+
+// Voeg de navigatiebalk HTML toe
+navbarPlaceholder.innerHTML = `
+    <nav id="navbar-placeholder">
+        <ul class="navbar-list">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">Over Ons</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </nav>
+`;
