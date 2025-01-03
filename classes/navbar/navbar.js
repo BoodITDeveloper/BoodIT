a3a710b95fbc07360cbf205deb62cb2f5a141b80
Lead Developer <BoodIT_Development@hotmail.com>
2025-01-03 13:28:39 +0100

    contact pagina gemaakt

--- classes/navbar/navbar.js
+++ classes/navbar/navbar.js
@@ -1,14 +1,18 @@
 const navbarPlaceholder = document.getElementById("navbar");
 
-
 // Voeg de navigatiebalk HTML toe
 navbarPlaceholder.innerHTML = `
     <nav id="navbar-placeholder">
+    <div class="navbar-container">
+            <!-- Logo -->
+            <div class="logo">
+            <a href="../homepage/homepage.html">
+                <img src="../../assets/android-chrome-192x192.png" alt="Logo" />
+            </div>
+            <!-- Navigatiebalk -->
         <ul class="navbar-list">
-            <li><a href="index.html">Home</a></li>
             <li><a href="about.html">Over Ons</a></li>
             <li><a href="../contact/contactpage.html">Contact</a></li>
         </ul>
     </nav>
 `;
-
